cbb058e29004ff45b2adc7ef19291f3731c57f04
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2023-09-30 15:41:38 +0500

    Update PlotAnimation.py

--- PlotAnimation.py
+++ PlotAnimation.py
@@ -6,12 +6,11 @@ import matplotlib.animation as animation
 from matplotlib.patches import Circle
 import numpy  as np
 
-data_file = "all_agents_data.json"
 
 fig, ax = plt.subplots()
 
-def read_data(data_file):
-    with open(data_file, "r") as f:
+def read_data():
+    with open(rf"{Results['InitPositions']}\run.json", "r") as f:
         all_data = json.load(f)
 
     # Create a dictionary to store agent data
@@ -44,28 +43,12 @@ def function(i, agent_data):
             circle = Circle((x_pos, y_pos), radius=5, edgecolor='black', facecolor='none', linewidth=0.1)
             plt.gca().add_patch(circle)
 
-            # Add lines showing velocity and acceleration
-            velocity = data_point.get("Velocity", [0, 0])
-            # acceleration = data_point.get("Acceleration", [0, 0])
-
-            # Calculate velocity magnitude
-            velocity_magnitude = np.linalg.norm(velocity)
-
-            # Check if velocity_magnitude is not zero
-            if velocity_magnitude > 0:
-                # Normalize velocity vector
-                normalized_velocity = velocity / velocity_magnitude
-
-                # Draw velocity arrow (green)
-                plt.arrow(x_pos, y_pos, normalized_velocity[0], normalized_velocity[1], head_width=0.2, head_length=0.2, fc='g', ec='g')
-
-            # # Draw acceleration line (blue)
-            # plt.arrow(x_pos, y_pos, acceleration[0], acceleration[1], head_width=0.2, head_length=0.3, fc='b', ec='b')
-
     plt.grid(True)
 
 
-agent_data, max_num_points = read_data(data_file)
-anim = animation.FuncAnimation(fig, function, fargs=(agent_data,), frames=max_num_points, interval=100, blit=False)
+
+PositionsFile = Results["InitPositions"] + "\run.json"
+agent_data, max_num_points = read_data()
+anim = animation.FuncAnimation(fig, function, fargs = (agent_data,), frames = max_num_points, interval = 100, blit=False)
 anim.save(animation_filename, writer="ffmpeg")
 plt.show()
