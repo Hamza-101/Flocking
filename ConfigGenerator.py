c5a7c4d3b80a5290111e02510d704425a5d3ec9c
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2023-09-30 15:40:51 +0500

    Update ConfigGenerator.py

--- ConfigGenerator.py
+++ ConfigGenerator.py
@@ -5,14 +5,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from Params import *
+from tqdm import tqdm
 
 TotalConfigs = 0
 
 class Agent:
     def __init__(self):
-        self.position = np.array([round(random.uniform(InitParams["X_Min"], InitParams["X_Max"]), 1), round(random.uniform(InitParams["Y_Min"], InitParams["Y_Max"]), 1)])
+        self.position = np.array([round(random.uniform(-SimulationVariables["X"], SimulationVariables["X"]), 1), round(random.uniform(SimulationVariables["Y"], SimulationVariables["Y"]), 1)])
         
-    #Get Connectedness
+    # Connectedness
     def get_neighbors(agent, agents, radius):
         neighbors = []
         
@@ -21,7 +22,7 @@ class Agent:
                 distance = np.linalg.norm(agent.position - other_agent.position)
                 if distance < radius:
                     neighbors.append(other_agent)
-                    return True # Stop searching for more neighbors once at least one is found within the radius
+                    return True
 
         return False
 
@@ -43,58 +44,49 @@ def get_agent_locations(agents):
     agent_locations = [agent.position.tolist() for agent in agents]
     return agent_locations
 
+
 def save_config(ConfigValidation, agent_locations):
     global TotalConfigs  
 
-    folder_name = "CorrectConfigs"
+    # Create the "Directory" folder if it doesn't exist
+    if not os.path.exists(Results["Directory"]):
+        os.makedirs(Results["Directory"])
 
-    if ConfigValidation == False:
-        folder_name = "IncorrectConfigs"
+    while True:
+        # Find the next available configuration number
+        folder_name = f"{Results['Directory']}/Config_{TotalConfigs}"
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+            break
+        TotalConfigs += 1
 
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-
-    while(os.path.exists(f"{folder_name}/Config_{TotalConfigs}.json") == True):
-        TotalConfigs = TotalConfigs + 1
-
-    filename = f"{folder_name}/Config_{TotalConfigs}.json"
-
+    # Save the agent locations to a JSON file in the configuration folder
+    filename = f"{folder_name}/config.json"
     with open(filename, "w") as f:
-        json.dump(agent_locations, f, cls=Encoder)  # Serialize agent_locations and write it to the file
+        json.dump(agent_locations, f, cls=Encoder)
         f.write("\n")
 
     return folder_name
 
-    # config_info = {
-    #     "CartesianLimits": CartesianLimits,
-    #     "DataParams": DataParams,
-    #     "InitParams": InitParams,
-    #     "AgentLocations": agent_locations
-    # }
-
-# config_info,
-
-
-
 
 def property_check(agents):
-    # Create a dictionary to store neighbors for each agent
+    # Dictionary to store neighbors for each agent
     agent_neighbors = {agent: [] for agent in agents}
 
     # Check safety and connectedness for each agent
     for agent in agents:
-        # Safe distance check
-        if not Agent.check_min_distance(agent, agents, DataParams["safe_distance"]):
+        # Safety distance check
+        if not Agent.check_min_distance(agent, agents, SimulationVariables["SafetyRadius"]):
             return False
 
         # Check connectedness within the neighborhood distance
         for other_agent in agents:
             if agent != other_agent:
                 distance = np.linalg.norm(agent.position - other_agent.position)
-                if distance < DataParams["Neighborhood"]:
+                if distance < SimulationVariables["NeighborhoodRadius"]:
                     agent_neighbors[agent].append(other_agent)
 
-    # Check if there are no neighbors within the neighborhood distance for any agent
+    # Check if there are no neighbors within the neighborhood distance for ally agent
     if any(not neighbors for neighbors in agent_neighbors.values()):
         return False
 
@@ -106,8 +98,8 @@ def plot_agents(agents, folder_name):
     x_coordinates = [agent.position[0] for agent in agents]
     y_coordinates = [agent.position[1] for agent in agents]
 
-    plt.figure(figsize=(CartesianLimits["X"] + 5, CartesianLimits["Y"] + 5))
-    plt.scatter(x_coordinates, y_coordinates, c='red', label='Agents', marker='o')
+    plt.figure(figsize=(SimulationVariables["X"] + 5, SimulationVariables["Y"] + 5))
+    plt.scatter(x_coordinates, y_coordinates, c = 'red', label = 'Agents', marker = 'o')
 
     plt.xlabel('X Axis')
     plt.ylabel('Y Axis')
@@ -118,24 +110,22 @@ def plot_agents(agents, folder_name):
     # Set equal aspect ratio for axis scaling (1:1)
     plt.axis("equal")
     
-    plt.xlim(InitParams["X_Min"], InitParams["X_Max"])
-    plt.ylim(InitParams["Y_Min"], InitParams["Y_Max"])
+    plt.xlim(-(SimulationVariables["X"]), SimulationVariables["X"])
+    plt.ylim(-(SimulationVariables["Y"]), SimulationVariables["Y"])
 
     plt.legend()
     plt.grid(True)
     plt.show()
 
-i = 0
 j = 0
-while(i<=10000):
-    agents = [Agent() for _ in range(InitParams["NumAgents"])]
+
+
+for i in tqdm(range(1, 1000)):
+    agents = [Agent() for _ in range(SimulationVariables["SimAgents"])]
 
     check_result = property_check(agents)
     AgentsData = get_agent_locations(agents)
-    folder_name = save_config(check_result, AgentsData)
-    if(check_result==True):
-        print(check_result)
-        j=j+1
-    i = i + 1
-print(j)    
-    
+    if check_result == True:
+        folder_name = save_config(check_result, AgentsData)
+        j = j + 1
+print(j)
